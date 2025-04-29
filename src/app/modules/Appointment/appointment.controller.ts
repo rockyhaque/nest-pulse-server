796874219cd529f17b78b8403c50607b40c1a230
Rocky Haque <rockyhaque99@gmail.com>
2025-04-29 19:18:23 +0600

    all schema done

---
+++ src/app/modules/Appointment/appointment.controller.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import { TAuthUser } from "../../interfaces/common";
+import { StatusCodes } from "http-status-codes";
+import sendResponse from "../../../shared/sendResponse";
+import { AppointmentService } from "./appointment.service";
+import pick from "../../../shared/pick";
+import { appointmentFilterableFields } from "./appointment.constant";
+
+
+const createAppointment = catchAsync(async (req: Request &{user?: TAuthUser} , res: Response) => {
+
+    const user = req.user;
+
+    const result = await AppointmentService.createAppointment(user as TAuthUser, req.body);
+
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: "Appointment booked successfully!",
+        data: result
+    })
+});
+
+const getMyAppointment = catchAsync(async (req: Request & { user?: TAuthUser }, res: Response) => {
+    const user = req.user;
+    const filters = pick(req.query, ['status', 'paymentStatus']);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+    const result = await AppointmentService.getMyAppointment(user as TAuthUser, filters, options);
+
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'My Appointment retrive successfully',
+        data: result
+    });
+});
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, appointmentFilterableFields)
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await AppointmentService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Appointment retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
+export const AppointmentController = {
+    createAppointment,
+    getMyAppointment,
+    getAllFromDB
+}
